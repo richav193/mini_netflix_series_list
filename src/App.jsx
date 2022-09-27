d5d066f1e16330c71fe810ef1568727bb2f6d684
Richa <richaverma13031975@gmail.com>
2022-09-27 13:00:47 +0530

    Added some jsx files and updated cards file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,57 @@
 import React from 'react';
-import Cards from "./Cards";
-import Sdata from './Sdata';   
+// import Cards from "./Cards";
+// import Sdata from './Sdata';
+import Netflix from './Netflix'; 
+import Amazon from './Amazon'; 
+
+const favseries="netflix";
+
+// const FavS=()=>{
+// if(favseries==="netflix"){
+//   //   return(
+//   //       <Cards 
+//   //       key={Sdata[1].id}
+//   //       imgsrc={Sdata[1].imgsrc}
+//   //       title={Sdata[1].title}
+//   //       sname={Sdata[1].sname}
+//   //       link={Sdata[1].link}
+//   // />
+//   //   );
+//  return  <Netflix/>
+// }
+// else{
+// //   return(
+// //     <Cards 
+// //     key={Sdata[4].id}
+// //     imgsrc={Sdata[4].imgsrc}
+// //     title={Sdata[4].title}
+// //     sname={Sdata[4].sname}
+// //     link={Sdata[4].link}
+// // />
+// // );
+//   return <Amazon/>
+// }
+// }
+
 
 const App = () => (
   <>
   <h1 className='heading_style'>List of Top 5 Netflix series in 2022</h1>
-  {Sdata.map((val,index)=> {
+  {/* {Sdata.map((val,index)=> {
     return(
         <Cards 
-        key={val.id}
+      key={val.id}
         imgsrc={val.imgsrc}
         title={val.title}
         sname={val.sname}
         link={val.link}
   />
     );
-  })} 
+  })}  */}
+
+  {/* <FavS/> */}
+
+  {favseries==="netflix" ? <Netflix/> : <Amazon/>}
   </>
 
 );
